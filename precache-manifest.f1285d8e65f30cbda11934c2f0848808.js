e5bd627b91a12d26db12b3f67f7ca160c7781367
Ritik Poshattiwar <ritik.poshattiwar@gmail.com>
2020-06-24 10:13:11 +0530

    Updates

---
+++ precache-manifest.f1285d8e65f30cbda11934c2f0848808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9c32512a10601b79a308b647a78a4d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "59c95ec794c7e7e2c034",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "1d320a4315c272e8ef9b",
+    "url": "/tic-tac-toe/static/js/2.fd9dbe35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.fd9dbe35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c95ec794c7e7e2c034",
+    "url": "/tic-tac-toe/static/js/main.926beb32.chunk.js"
+  },
+  {
+    "revision": "28dfba9420967d301740",
+    "url": "/tic-tac-toe/static/js/runtime-main.4c12a8d1.js"
+  }
+]);
